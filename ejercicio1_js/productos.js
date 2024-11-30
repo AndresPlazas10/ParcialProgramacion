cbc8d81787e3aed76c91faefe7e7d3c8eac7a9df
Sala D19 <CSalaD19@uniboyaca.edu.co>
2024-11-30 09:58:04 -0500

    Parcial programacion

---
+++ ejercicio1_js/productos.js
@@ -0,0 +1,8 @@
+$vueltas = document.getElementById('vueltas');
+$tiempo = document.getElementById('tiempo');
+
+
+ function validar(){
+    $corredor = 0;
+    $data = [$corredor, $vueltas , $tiempo]
+}
